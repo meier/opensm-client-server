9678c027f08261237342182fabbe682a0711be5b
Tim Meier <meier3@llnl.gov>
2015-08-05 11:36:17 -0700

    removed misc folder

--- src/dist/misc/genJavaDoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# generates the javadocs for the OpenSM Java Plugin Client/Server Project
-#
-# invoked from within the opensm-client-server/misc directory
-#
-# tam 7-11-2012
-#
-
-javadoc @misc/jdocOptions.txt @misc/packageList.txt
