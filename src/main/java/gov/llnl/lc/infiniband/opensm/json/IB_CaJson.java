f1e18b56de49d4d578a5d1803e31634005661081
Tim Meier <meier3@llnl.gov>
2018-06-06 10:57:09 -0700

    initial JSON support

---
+++ src/main/java/gov/llnl/lc/infiniband/opensm/json/IB_CaJson.java
@@ -0,0 +1,91 @@
+/************************************************************
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * Written by Timothy Meier, meier3@llnl.gov, All rights reserved.
+ * LLNL-CODE-673346
+ *
+ * This file is part of the OpenSM Monitoring Service (OMS) package.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (as published by
+ * the Free Software Foundation) version 2.1 dated February 1999.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * OUR NOTICE AND TERMS AND CONDITIONS OF THE GNU GENERAL PUBLIC LICENSE
+ *
+ * Our Preamble Notice
+ *
+ * A. This notice is required to be provided under our contract with the U.S.
+ * Department of Energy (DOE). This work was produced at the Lawrence Livermore
+ * National Laboratory under Contract No.  DE-AC52-07NA27344 with the DOE.
+ *
+ * B. Neither the United States Government nor Lawrence Livermore National
+ * Security, LLC nor any of their employees, makes any warranty, express or
+ * implied, or assumes any liability or responsibility for the accuracy,
+ * completeness, or usefulness of any information, apparatus, product, or
+ * process disclosed, or represents that its use would not infringe privately-
+ * owned rights.
+ *
+ * C. Also, reference herein to any specific commercial products, process, or
+ * services by trade name, trademark, manufacturer or otherwise does not
+ * necessarily constitute or imply its endorsement, recommendation, or favoring
+ * by the United States Government or Lawrence Livermore National Security,
+ * LLC. The views and opinions of authors expressed herein do not necessarily
+ * state or reflect those of the United States Government or Lawrence Livermore
+ * National Security, LLC, and shall not be used for advertising or product
+ * endorsement purposes.
+ *
+ *        file: IB_CaJson.java
+ *
+ *  Created on: May 31, 2018
+ *      Author: meier3
+ ********************************************************************/
+package gov.llnl.lc.infiniband.opensm.json;
+
+import java.util.ArrayList;
+
+/**********************************************************************
+ * Describe purpose and responsibility of IB_CaJson
+ * <p>
+ * @see  related classes and interfaces
+ *
+ * @author meier3
+ * 
+ * @version May 31, 2018 1:35:37 PM
+ **********************************************************************/
+public class IB_CaJson
+{
+  private String name;
+  private String width;
+  private String speed;
+  private ArrayList<IB_PortJson> ports = new ArrayList<IB_PortJson>();
+
+  /************************************************************
+   * Method Name:
+   *  IB_CaJson
+  **/
+  /**
+   * Describe the constructor here
+   *
+   * @see     describe related java objects
+   *
+   ***********************************************************/
+  public IB_CaJson()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+}
